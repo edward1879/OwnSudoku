bc82404f25491c3fcc6adbd246cc90e9f90aeac6
SunnyChan <edward1879314@gmail.com>
2016-11-25 20:16:49 +0800

    added numberPad and can retrieve the selected number

--- app/src/main/java/com/example/android/ownsudoku/MainActivity.java
+++ app/src/main/java/com/example/android/ownsudoku/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,17 +14,26 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TextView for the cells
     TextView[][] cell;
+    //for showing the selected number
+    TextView tv_selectedNum;
+    //Buttons for the number pad
+    Button[] numberPad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cell = new TextView[4][4];
-        bindTextViews();
+        setUpUI();
         getTextFromAllTV();
 
+    }
+
+    private void setUpUI(){
+        bindTextViews();
+        bindNumberButtons();
     }
 
     private void getTextFromAllTV() {
@@ -34,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
                     curCell.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
+                            String selectedNum = (String) tv_selectedNum.getText();
+                            curCell.setText(selectedNum);
 //                            curCell.setBackgroundColor(Color.GRAY);
 //                            Toast.makeText(MainActivity.this, curCell.getText(), Toast.LENGTH_SHORT).show();
                         }
@@ -43,7 +55,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void bindNumberButtons(){
+        int numberOfButtons = 4;
+        numberPad = new Button[numberOfButtons];
+        for (int i = 0;i<numberOfButtons;i++){
+            String buttonName = "btn_num_"+(i+1);
+            int id = getStringIdentifier(this,buttonName);
+            logCat("Button id: "+id);
+            numberPad[i] = (Button) findViewById(id);
+        }
+        setOnClickForNumberButtons();
+    }
+
+    private void setOnClickForNumberButtons(){
+        if (numberPad!=null){
+            for (int i = 0;i<numberPad.length;i++){
+                final Button curButton = numberPad[i];
+                curButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        tv_selectedNum.setText(curButton.getText());
+                    }
+                });
+            }
+        }
+    }
+
+
     private void bindTextViews() {
+        //bind the textViews
+        tv_selectedNum = (TextView) findViewById(R.id.tv_selectedNum);
+        cell = new TextView[4][4];
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 4; col++) {
                 logCat(row + "," + col);
